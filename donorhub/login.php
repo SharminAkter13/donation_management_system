1947633b062878ea19ad998498a761500131d945
Sharmin Akter <sharminakter13092000@gmail.com>
2025-12-21 12:50:10 +0600

    dms

--- donorhub/login.php
+++ donorhub/login.php
@@ -79,6 +79,7 @@ if (isset($_POST['submit'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>DonorHub | Log in </title>
+     <link href="assets/img/img/main-logo.png" rel="icon">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
--- donorhub/master.php
+++ donorhub/master.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AdminLTE 3 | Dashboard 2</title>
+   <link href="assets/img/img/main-logo.png" rel="icon">
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
